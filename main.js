23c769e22fae1306a844d2718ab2f2ece18e4595
Jung Seonghun <80201773+seonghun-dev@users.noreply.github.com>
2022-09-27 16:53:38 +0900

    Feat: Add send img

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 const { XMLParser } = require('fast-xml-parser');
 const parser = new XMLParser();
-var { createPost, createFile } = require('./src/recent-post');
+var { createPost } = require('./src/recent-post');
 
 async function getDataFromMediumFeed(blogname) {
     try {
@@ -34,22 +34,26 @@ const createShortDescription = (contents) => {
 
 const getData = async (blogname) => {
     r = await getDataFromMediumFeed(blogname);
-    console.log(r);
     const result = parser.parse(r);
     const items = result.rss.channel.item[0];
-    console.log(items);
     const title = result.rss.channel.item[0].title;
     const category = result.rss.channel.item[0].category;
     const description = createShortDescription(result.rss.channel.item[0]['content:encoded']);
     const pubDate = result.rss.channel.item[0].pubDate.split(',')[1].split(' ')[1] + ' ' + result.rss.channel.item[0].pubDate.split(',')[1].split(' ')[2];
     const username = result.rss.channel.title.replace("Stories by ", "").replace(" on Medium", "");
-    console.log(username);
-    console.log(title);
-    console.log(category);
-    console.log(description);
-    console.log(pubDate);
     const post = createPost(username, pubDate, title, description, category);
-    createFile(post);
+    return post;
 }
 
-getData("barackobama");
+
+module.exports = async (req, res) => {
+    try {
+        const { name } = req.query;
+        const result = await getData(name);
+        res.setHeader('Content-Type', 'image/svg+xml');
+        res.status(200).send(result);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+
+}
